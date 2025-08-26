c6dfcb87392b3e6f70da2662d41bee34b853e6b8
Krishna4K2 <krishna.ch.tech@gmail.com>
2025-08-26 17:02:46 +0530

    recommendation service

---
+++ services/recommendation/api/api.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"math/rand"
+	"recommendation/data" // replace with your actual project name
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetOrigamiOfTheDay(c *gin.Context) {
+	origamis := data.GetDailyOrigami()
+	rand.Seed(time.Now().Unix())                          // initialize global pseudo random generator
+	selectedOrigami := origamis[rand.Intn(len(origamis))] // get a random origami
+
+	c.JSON(200, selectedOrigami)
+}
+
+func StartAPI() {
+	r := gin.Default()
+	r.GET("/origami-of-the-day", GetOrigamiOfTheDay)
+
+	// TODO: Add more routes as needed
+
+	r.Run(":8080") // start the server on port 8080
+}
